e2178fd8e0d92ce1c280d5cd2c024b387522d62d
Connor McCutcheon <connormc7@gmail.com>
2021-12-09 06:22:40 +0000

    Adding back old example

---
+++ cmd/example/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"biplane.build"
+	"biplane.build/mixins"
+	"biplane.build/server"
+	"github.com/gorilla/mux"
+)
+
+type Account struct {
+	mixins.Model
+
+	Name string
+}
+
+func (a Account) Greeting() Greeting {
+	return Greeting{
+		Message: fmt.Sprintf("Hello %s", a.Name),
+	}
+}
+
+type Greeting struct {
+	mixins.View
+
+	Message string
+}
+
+type App struct {
+	mixins.Controller
+}
+
+func (c App) Routes(r *mux.Router) {
+	s := r.PathPrefix("/").Subrouter()
+
+	s.Methods("POST").Path("/create").
+		HandlerFunc(c.CreateAccount)
+}
+
+func (c App) CreateAccount(w http.ResponseWriter, r *http.Request) {
+	var a Account
+	c.ParseJSON(r, &a)
+
+	_, err := c.Objects().New(c.User(r), "Account", &a)
+	if err != nil {
+		c.Fail(w, err)
+		return
+	}
+
+	c.Display(w, a.Greeting())
+}
+
+func main() {
+	biplane.TakeOff(server.Config{
+		Port:    8080,
+		Routers: []server.Router{new(App)},
+	})
+}
